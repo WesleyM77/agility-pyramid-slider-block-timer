caa42682fad42b32390f83020db04be29d3829d6
WesleyM77 <ShadowWesley77@hotmail.com>
2021-11-16 21:50:56 -0500

    Remove license text in favor of repo license. Remove unused code.

--- src/main/java/com/agilitypyramidsliderblocktimer/Npcs.java
+++ src/main/java/com/agilitypyramidsliderblocktimer/Npcs.java
@@ -5,23 +5,11 @@ import com.google.common.collect.ImmutableSet;
 
 import java.util.Map;
 import static net.runelite.api.NpcID.*;
-import static net.runelite.api.NullNpcID.*;
 
 import java.util.Set;
-import static net.runelite.api.NullObjectID.NULL_10872;
-import static net.runelite.api.NullObjectID.NULL_10873;
 
 public class Npcs
 {
-
-	static final Set<Integer> NPC_IDS = ImmutableSet.of(
-		// Agility Pyramid
-		PYRAMID_BLOCK, PYRAMID_BLOCK_5788,
-		// Hallowed Sepulchre
-		NULL_9672, NULL_9673, NULL_9674,  // arrows
-		NULL_9669, NULL_9670, NULL_9671   // swords
-	);
-
 	static final Set<Integer> TRAP_NPC_IDS = ImmutableSet.of(
 		// Agility pyramid
 		PYRAMID_BLOCK, PYRAMID_BLOCK_5788
@@ -40,15 +28,5 @@ public class Npcs
 	static final Map<Integer, String> TRAP_RESTING_TIME = ImmutableMap.of(
 		PYRAMID_BLOCK, "PT4.8S",
 		PYRAMID_BLOCK_5788, "PT6.0S"
-	);
-
-	static final Map<Integer, Integer> TRAP_RESTING_TILE = ImmutableMap.of(
-		PYRAMID_BLOCK, NULL_10873,
-		PYRAMID_BLOCK_5788, NULL_10872
-	);
-
-	static final Set<Integer> SEPULCHRE_NPC_IDS = ImmutableSet.of(
-		NULL_9672, NULL_9673, NULL_9674,  // arrows
-		NULL_9669, NULL_9670, NULL_9671   // swords
 	);
 }
